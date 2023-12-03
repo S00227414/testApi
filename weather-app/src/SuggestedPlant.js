331185450fcbef0889795420e263faf3379b06ee
s00227414 <s00227414@atu.ie>
2023-12-03 21:50:08 +0000

    Got suggested plants working.

--- weather-app/src/SuggestedPlant.js
+++ weather-app/src/SuggestedPlant.js
@@ -1,45 +1,64 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios'; // Import Axios for making HTTP requests
 
 const SuggestedPlant = () => {
   const [selectedPlant, setSelectedPlant] = useState(null);
-  const seasons = ['Spring', 'Summer', 'Autumn', 'Winter'];
-  const currentSeason = seasons[0]; // Assuming it's Autumn for example purposes
+  const [currentSeason, setCurrentSeason] = useState('');
 
   useEffect(() => {
-    const fetchPlant = async () => {
+    const determineCurrentSeason = () => {
+      const date = new Date();
+      const month = date.getMonth() + 1;
+      let season;
+
+      if (month >= 3 && month <= 5) {
+        season = 'Spring';
+      } else if (month >= 6 && month <= 8) {
+        season = 'Summer';
+      } else if (month >= 9 && month <= 11) {
+        season = 'Autumn';
+      } else {
+        season = 'Winter';
+      }
+
+      setCurrentSeason(season);
+    };
+
+    determineCurrentSeason();
+  }, []);
+
+  useEffect(() => {
+    const fetchPlantData = async () => {
       try {
-        // Choose plant_id range based on the season
-        let minPlantId = 0;
-        let maxPlantId = 0;
-
-        if (currentSeason === 'Spring') {
-          minPlantId = 1;
-          maxPlantId = 8;
-        } else if (currentSeason === 'Summer') {
-          minPlantId = 9;
-          maxPlantId = 18;
-        } else if (currentSeason === 'Autumn') {
-          minPlantId = 19;
-          maxPlantId = 24;
-        } else if (currentSeason === 'Winter') {
-          minPlantId = 25;
-          maxPlantId = 25;
-        }
-
-        // Generate a random plant_id within the specified range
-        const randomPlantId = Math.floor(Math.random() * (maxPlantId - minPlantId + 1)) + minPlantId;
-
-        // Now you have the random plant ID to fetch from the API
-        const response = await fetch(`https://bmhnryodyk.execute-api.eu-west-1.amazonaws.com/v1/${randomPlantId}`);
-        const data = await response.json();
-        setSelectedPlant(data);
+        const response = await axios.get(`https://bmhnryodyk.execute-api.eu-west-1.amazonaws.com/v1/${generateRandomPlantId(currentSeason)}`);
+        const responseBody = JSON.parse(response.data.body); // Parse the body string as JSON
+        setSelectedPlant(responseBody);
       } catch (error) {
         console.error('Error fetching plant data:', error);
       }
     };
 
-    fetchPlant();
+    fetchPlantData();
   }, [currentSeason]);
+
+  const generateRandomPlantId = (season) => {
+    let minId, maxId;
+    if (season === 'Spring') {
+      minId = 1;
+      maxId = 8;
+    } else if (season === 'Summer') {
+      minId = 9;
+      maxId = 18;
+    } else if (season === 'Autumn') {
+      minId = 19;
+      maxId = 24;
+    } else if (season === 'Winter') {
+      minId = 25;
+      maxId = 25;
+    }
+
+    return Math.floor(Math.random() * (maxId - minId + 1)) + minId;
+  };
 
   return (
     <div>
